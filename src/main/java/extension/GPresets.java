aef39402dcfc7169297c68e282a6a8980bbbcaac
WiredSpast <wiredspast@gmail.com>
2026-01-19 13:52:03 +0100

    1.2.8 -> 1.2.9
    Signed-off-by: WiredSpast <wiredspast@gmail.com>

--- src/main/java/extension/GPresets.java
+++ src/main/java/extension/GPresets.java
@@ -53,7 +53,7 @@ import java.util.stream.Collectors;
 @ExtensionInfo(
         Title =  "G-Presets",
         Description =  "Never do anything twice",
-        Version =  "1.2.8",
+        Version =  "1.2.9",
         Author =  "sirjonasxx"
 )
 public class GPresets extends ExtensionForm {
